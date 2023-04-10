4134840feba61e26d485349e0ca8423ee3a38398
Muhammadqodir <111755347+Azamov-Muhammadqodir@users.noreply.github.com>
2023-04-10 18:29:26 +0500

    Architecture created

---
+++ BookShop.Presentation/BookShop.Presentation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BookShop.Presentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
