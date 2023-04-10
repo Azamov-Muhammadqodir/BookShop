7b4e7171ae8beddacaedc0bb1399c76918534577
Muhammadqodir <111755347+Azamov-Muhammadqodir@users.noreply.github.com>
2023-04-10 20:12:00 +0500

    Created Models. User, Book, Savat

---
+++ BookShop.Presentation/BookShop.Domain/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using BookShop.Domain.States;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.Domain.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public double Price { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public BookCategoryEnum? Category { get; set; }
+
+    }
+}
